25d7cfe455303ecd5e22360318ebb39b8cddff8f
CARLOS-Datablitz <venus.cleme@gmail.com>
2025-09-15 14:28:31 -0500

    add font rds

---
+++ Attachments/attachments_base_marcos.py
@@ -0,0 +1,27 @@
+#1st Run
+screen -dRR
+cd /usr/sap/SAPBusinessOne/B1_SHF/ && ls -lha
+mkdir LIVRECOLD/ && ls -lha
+cd LIVRECOLD
+mkdir Word/ Excel/ Pictures/ Attachments/ Extensions/ XML/
+chmod -R 777 LIVRECOLD/ && ls -lha
+chown -R b1service0:b1service0 LIVRECOLD/ && ls -lha
+
+
+#2nd run
+cd /mnt
+mkdir Attachments/
+chmod -R 777 Attachments/
+chown -R b1service0:b1service0 Attachments/
+ls -lha && df -h
+
+# echo '//sld/B1_BHF/ONEID/Attachments       /mnt/Attachments       cifs rw,guest,uid=b1service0,gid=b1service0,vers=3.0,iocharset=utf8,file_mode=0777,dir_mode=0777,noperm,nounix,x-systemd.automount 0 0'>>/etc/fstab
+
+#this is the only one that you execute
+echo '//sld/B1_SHF/LIVRECOLD/Attachments       /mnt/Attachments       cifs rw,guest,uid=b1service0,gid=b1service0,vers=3.0,iocharset=utf8,file_mode=0777,dir_mode=0777,noperm,nounix,x-systemd.automount 0 0'>>/etc/fstab
+
+#check the fstab file and modify it if necessary 
+cat /etc/fstab
+vim /etc/fstab
+mount -a -v
+df -h | grep Attachments
---
+++ Attachments/livercol_attch.sh
@@ -0,0 +1,25 @@
+#1st Run
+screen -dRR
+cd /usr/sap/SAPBusinessOne/B1_SHF/ && ls -lha
+mkdir LETRAMEIO/ && ls -lha
+cd LETRAMEIO
+mkdir Word/ Excel/ Pictures/ Attachments/ Extensions/ XML/
+chmod -R 777 LETRAMEIO/ && ls -lha
+chown -R b1service0:b1service0 LETRAMEIO/ && ls -lha
+
+
+#2nd run
+cd /mnt
+mkdir Attachments/
+chmod -R 777 Attachments/
+chown -R b1service0:b1service0 Attachments/
+ls -lha && df -h
+
+#this is the only one that you execute
+echo '//sld/B1_SHF/LETRAMEIO/Attachments       /mnt/Attachments       cifs rw,guest,uid=b1service0,gid=b1service0,vers=3.0,iocharset=utf8,file_mode=0777,dir_mode=0777,noperm,nounix,x-systemd.automount 0 0'>>/etc/fstab
+
+#check the fstab file and modify it if necessary 
+cat /etc/fstab
+vim /etc/fstab
+mount -a -v
+df -h | grep Attachments
