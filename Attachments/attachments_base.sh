a5bf0d509674cde969293569dc35d56e6b88bf65
CARLOS-Datablitz <venus.cleme@gmail.com>
2025-12-31 11:17:18 -0500

    Ultimo dia de 2025

--- Attachments/attachments_base.sh
+++ Attachments/attachments_base.sh
@@ -77,3 +77,27 @@ cat /etc/fstab
 mount -a -v
 df -h
 df -h | grep Exdoc
+
+===================== Attachment basico ===================
+
+# ssh root@sld
+zypper install -y tree
+screen -dRR
+cd /usr/sap/SAPBusinessOne/B1_SHF/
+mkdir -p Attachments/
+chmod -R 777 Attachments/
+chown -R b1service0:b1service0 Attachments/
+
+cd /mnt
+mkdir -p Attachments/
+chmod -R 777 Attachments/
+chown -R b1service0:b1service0 Attachments/ && ls -lha
+
+echo '//sld/B1_SHF//Attachments/                  /mnt/Attachments        cifs rw,guest,uid=b1service0,gid=b1service0,vers=3.0,iocharset=utf8,file_mode=0777,dir_mode=0777,noperm,nounix,x-systemd.automount 0 0'>>/etc/fstab
+
+cat /etc/fstab
+# vim /etc/fstab
+mount -a -v
+df -h
+df -h | grep Attachments
+
--- Linux/comandos_linux.sh
+++ Linux/comandos_linux.sh
@@ -1,5 +1,5 @@
 ------------------------------------
-
+#buscar en el directorio actual(.) el archivo
 ~$ find . -name <filename*>
 
 which <program name eg. file>
@@ -8,7 +8,22 @@ file
 echo $PATH
 
 ------------------------------------
-pwd como variable:
+#pwd como variable:
 $ cat $(pwd)/*
 
 ------------------------------------
+#ver archivos ocultos: los recursos que empiezan por un . no se ven ka menos que uses:
+$ ls -a inhere/
+
+------------------------------------
+# xargs: en forma paralela ejecutar comandos en base al output de un comando ejecutado anteriormente:
+$ find . -name .hidden | xargs cat
+$ find . -type f | xargs grep "leaving"
+------------------------------------
+# mostrar solo unas lineas del output de cat
+$ cat /etc/passwd | head -n 2
+$ cat /etc/passwd | tail -n 2
+$ cat /etc/passwd | awk 'NR==2'
+------------------------------------
+$ cat /usr/share/rockyou.txt | grep ^hola # busca explicitamente todas las palabras que inican con hola
+
